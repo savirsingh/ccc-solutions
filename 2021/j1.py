9c9b64459f798a0b51af6f0bbf6aee87c12f6f86
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-17 19:53:52 -0400

    Create j1.py

---
+++ 2021/j1.py
@@ -0,0 +1,25 @@
+# code by savir singh
+
+x = int(input())
+y = 0
+
+if x == 100:
+    y = 1
+
+elif x < 100:
+    y = 2
+
+else:
+    pass
+
+if y == 0:
+    print(5*x-400)
+    print("-1")
+
+elif y == 1:
+    print(5*x-400)
+    print("0")
+
+else:
+    print(5*x-400)
+    print("1")
