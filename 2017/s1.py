da3f206457783ff10fdaa5e332ab0051fc4c9922
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-24 12:33:34 -0400

    Create s1.py

---
+++ 2017/s1.py
@@ -0,0 +1,29 @@
+# code by savir singh
+
+n = int(input())
+sw = input().split(" ")
+se = input().split(" ")
+ses = 0
+sws = 0
+
+swifts = []
+semaphores = []
+count = 0
+days = 0
+
+for i in range(n):
+    swifts.append(int(sw[i]))
+    semaphores.append(int(se[i]))
+
+for i in range(len(sw)):
+    sws += swifts[i]
+    ses += semaphores[i]
+    count += 1
+    if sws == ses and count == len(swifts):
+        days = len(swifts)
+    elif sws == ses and count != len(swifts):
+        days = swifts.index(swifts[i])+1
+
+    
+print(days)
+
