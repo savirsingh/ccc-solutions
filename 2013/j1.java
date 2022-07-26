420a3676457386f2315d1156a2c78331e00f073f
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-26 18:08:30 -0400

    Create j1.java

---
+++ 2013/j1.java
@@ -0,0 +1,15 @@
+// code by savir singh
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in1 = new Scanner(System.in);
+        int y = in1.nextInt();
+        int m = in1.nextInt();
+        int o = m-y;
+
+        System.out.println(m+o);
+    }
+}
