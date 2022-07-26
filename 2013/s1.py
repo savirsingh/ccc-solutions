d32eb7407f99f6b544fc2901d702d35f2f99126e
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-26 18:54:01 -0400

    Create s1.py

---
+++ 2013/s1.py
@@ -0,0 +1,10 @@
+x = int(input())
+
+def nextnum(year):
+    while True:
+        year+=1 
+        a = list(str(year))
+        s = set(a)
+        if len(s) == len(a):
+            return year
+print(nextnum(x))
