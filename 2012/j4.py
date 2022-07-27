9b3bf52ba43784ff741ea5f8a025bcf70939c70a
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-26 21:55:32 -0400

    Create j4.py

---
+++ 2012/j4.py
@@ -0,0 +1,14 @@
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+k = int(input())
+code = list(input())
+outputlist = []
+
+for i in range(len(code)):
+    p = code.index(code[i])+1
+    shift = 3*p+k
+    cindex = alphabet.index(code[i])
+    code[code.index(code[i])] = " "
+    outputlist.append(alphabet[cindex-shift])
+
+print(''.join(outputlist))
