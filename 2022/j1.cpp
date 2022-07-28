97a88f353538c769df94c2abf117eb01363f2ca1
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-28 12:36:27 -0400

    Create j1.cpp

---
+++ 2022/j1.cpp
@@ -0,0 +1,17 @@
+// code by savir singh
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+    int y;
+    int z;
+    cin >> x;
+    cin >> y;
+
+    z = 8*x+3*y;
+    cout << z-28;
+    return 0;
+}
