f84db40331301c982c5bc0a2fdc40a2b08f48477
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-29 12:09:26 -0400

    Create j1.cpp

---
+++ 2010/j1.cpp
@@ -0,0 +1,37 @@
+// code by savir singh
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n==1){
+        cout << "1";
+    }
+    else if (n==2) {
+        cout << "2";
+    }
+    else if (n==3) {
+        cout << "2";
+    }
+    else if (n==4) {
+        cout << "3";
+    }
+    else if (n==5) {
+        cout << "3";
+    }
+    else if (n==6) {
+        cout << "3";
+    }
+    else if (n==7) {
+        cout << "2";
+    }
+    else if (n==8) {
+        cout << "2";
+    }
+    else {
+        cout << "1";
+    }
+}
