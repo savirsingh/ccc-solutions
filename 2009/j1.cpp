9e50d8fbcb683687df9524ebf4741bacd1a8da1b
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-30 11:24:57 -0400

    Create j1.cpp

---
+++ 2009/j1.cpp
@@ -0,0 +1,11 @@
+// code by savir singh
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    cout << "The 1-3-sum is " + to_string(91+n1+n2*3+n3);
+}
