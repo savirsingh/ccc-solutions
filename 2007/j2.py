6e29af68feb0fd41a5eca13d686b306bbe2032ea
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-30 20:54:59 -0400

    Create j2.py

---
+++ 2007/j2.py
@@ -0,0 +1,33 @@
+# code by savir singh
+
+notcompleted = True
+
+while notcompleted:
+    line = input()
+    if line == "CU":
+        print("see you")
+    elif line == ":-)":
+        print("I'm happy")
+    elif line == ":-(":
+        print("I'm unhappy")
+    elif line == ";-)":
+        print("wink")
+    elif line == ":-P":
+        print("stick out my tongue")
+    elif line == "(~.~)":
+        print("sleepy")
+    elif line == "TA":
+        print("totally awesome")
+    elif line == "CCC":
+        print("Canadian Computing Competition")
+    elif line == "CUZ":
+        print("because")
+    elif line == "TY":
+        print("thank-you")
+    elif line == "YW":
+        print("you're welcome")
+    elif line == "TTYL":
+        print("talk to you later")
+        notcompleted = False
+    else:
+        print(line)
