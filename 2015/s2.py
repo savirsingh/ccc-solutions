17a8b14215c09def4df3da8d6ea9007ee8d2501d
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-05 12:49:07 -0400

    Create s2.py

---
+++ 2015/s2.py
@@ -0,0 +1,30 @@
+# code by savir singh
+
+# returns TLE cuz Python is slowwwww
+# code works fully though
+
+j = int(input())
+a = int(input())
+jerseys = []
+athletes = []
+requests = 0
+orders = ['S', 'M', 'L']
+taken = []
+
+for i in range(j):
+    jer = input()
+    jerseys.append(jer)
+
+for c in range(a):
+    ath = input().split(" ")
+    athletes.append(ath)
+
+for k in range(len(athletes)):
+    thejersey = jerseys[int(athletes[k][1])-1]
+    correctjersey = athletes[k][0]
+    if thejersey == correctjersey or orders.index(thejersey) > orders.index(correctjersey):
+        if int(athletes[k][1]) not in taken:
+            requests += 1
+        taken.append(int(athletes[k][1]))
+
+print(requests)
