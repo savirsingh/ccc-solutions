b379ff8578872f96042831de9efd3da97eb1af65
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-18 18:29:05 -0400

    Create j1.py

---
+++ 2002/j1.py
@@ -0,0 +1,115 @@
+# code by savir singh
+# optimized for dmoj
+
+num = int(input())
+
+if num == 0:
+    print(" * * *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print(" * * *".rstrip())
+
+if num == 1:
+    print("")
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("")
+
+if num == 2:
+    print(" * * *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print(" * * *".rstrip())
+    print("*".rstrip())
+    print("*".rstrip())
+    print("*".rstrip())
+    print(" * * *".rstrip())
+
+if num == 3:
+    print(" * * *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print(" * * *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print(" * * *".rstrip())
+
+if num == 4:
+    print("")
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print(" * * *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("")
+
+if num == 5:
+    print(" * * *".rstrip())
+    print("*".rstrip())
+    print("*".rstrip())
+    print("*".rstrip())
+    print(" * * *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print(" * * *".rstrip())
+
+if num == 6:
+    print(" * * *".rstrip())
+    print("*".rstrip())
+    print("*".rstrip())
+    print("*".rstrip())
+    print(" * * *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print(" * * *".rstrip())
+
+if num == 7:
+    print(" * * *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("")
+
+if num == 8:
+    print(" * * *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print(" * * *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print(" * * *".rstrip())
+
+if num == 9:
+    print(" * * *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print("*     *".rstrip())
+    print(" * * *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print("      *".rstrip())
+    print(" * * *".rstrip())
+    
