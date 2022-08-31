b788cbeb43a6b8d9142d5ce0d93deb09eac68d77
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-08-31 15:15:52 -0400

    Update j4.py

--- 2013/j4.py
+++ 2013/j4.py
@@ -1,17 +1,20 @@
 t = int(input())
 c = int(input())
+maxchores = 0
 chores = []
-counter = 0
 
 for i in range(c):
-    chore = int(input())
-    chores.append(chore)
+    a = int(input())
+    chores.append(a)
 
 chores.sort()
 
-for j in range(c):
-    if chores[j] <= t:
-        t -= chores[j]
-        counter += 1
+for j in range(len(chores)):
+    if chores[0] <= t:
+        maxchores+=1
+    else:
+        break
+    t-=chores[0]
+    del chores[0]
 
-print(counter)
+print(maxchores)
