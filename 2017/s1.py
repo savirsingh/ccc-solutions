fc2d205077f8efcb7146d38b3f3487cf1c4bf196
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-01 15:33:57 -0400

    Update s1.py

--- 2017/s1.py
+++ 2017/s1.py
@@ -1,18 +1,25 @@
 # code by savir singh
 
-n = int(input())
-swifts = list(map(int, input().split(" ")))
-semaphores = list(map(int, input().split(" ")))
-ses = 0
-sws = 0
+import sys
 
-days = 0
+n = int(sys.stdin.readline())
+swifts = []
+semaphores = []
+swifts = list(map(int, sys.stdin.readline().split()))
+semaphores = list(map(int, sys.stdin.readline().split()))
 
-for i in range(len(swifts)):
-    sws += swifts[i]
-    ses += semaphores[i]
-    if sws == ses:
-        days = i+1
+k = 1
+same = [0]
+sw = swifts[0]
+se = semaphores[0]
+for s in range(1, n+1):
+    try:
+        if sw==se:
+            same.append(k)
+        sw+=swifts[s]
+        se+=semaphores[s]
+        k+=1
+    except:
+        pass
 
-    
-print(days)
+print(max(same))
