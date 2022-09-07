e0327976ccdd5797b6e8458d9e49f1d16c996ede
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-07 18:46:04 -0400

    Create s3.cpp

---
+++ 2015/s3.cpp
@@ -0,0 +1,35 @@
+// code by savir singh
+// 6/15 - TLE
+
+#include <bits/stdc++.h>
+#define ll long long
+#define del erase
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll g, p;
+    cin >> g >> p;
+    vector<ll> occupied;
+    bool stop=false;
+    ll canland=0;
+    for (int i=0; i<p; i++) {
+        ll a;
+        cin >> a;
+        if (stop==false) {
+            while (a--) {
+                if (find(occupied.begin(), occupied.end(), a) == occupied.end()) {
+                    occupied.push_back(a);
+                    canland++;
+                    stop=false;
+                    break;
+                }
+                else {
+                    stop=true;
+                }
+            }
+        }
+    }
+    cout << canland;
+}
