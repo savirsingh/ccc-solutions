97257d5ccc3dd9a8b368fc4bb1b2a773eb63a70c
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-08 16:16:56 -0400

    Update s1.cpp

--- 2016/s1.cpp
+++ 2016/s1.cpp
@@ -1,5 +1,6 @@
 // code by savir singh
-// 9/15 - WA
+// 15/15 on DMOJ & CCC Grader
+// this was a hard problem lol
 
 #include <bits/stdc++.h>
 #define ll long long
@@ -12,7 +13,19 @@ int main() {
     bool stop=false;
     int ast = count(b.begin(), b.end(), '*');
     for (int i=0; i<a.length(); i++) {
-        if (count(a.begin(), a.end(), a[i]) != count(b.begin(), b.end(), a[i])) {
+        if (count(b.begin(), b.end(), '*')==b.length()) {
+            output = "A";
+            break;
+        }
+        else if (b[i]!='*' && find(a.begin(), a.end(), b[i])==a.end()) {
+            output = "N";
+            break;
+        }
+        else if (count(a.begin(), a.end(), a[i])<count(b.begin(), b.end(), a[i])) {
+            output = "N";
+            break;
+        }
+        else if (count(a.begin(), a.end(), a[i]) != count(b.begin(), b.end(), a[i])) {
             for (int j=1; j<=ast; j++) {
                 if (count(a.begin(), a.end(), a[i])==count(b.begin(), b.end(), a[i])+j) {
                     output = "A";
