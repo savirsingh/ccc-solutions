4ac747483136722348527575846432dbaed37dfb
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-10 17:32:51 -0400

    Update s2.cpp

--- 2021/s2.cpp
+++ 2021/s2.cpp
@@ -10,13 +10,17 @@ using namespace std;
 
 int32_t main()
 {
+    // fast i/o
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    // initialize everything and read m, n, and k
     ll m, n, k;
     cin >> m >> n >> k;
     vector<ll> rows (m, 0);
     vector<ll> cols (n, 0);
     ll gold_squares=0;
+    // read r/c & number and add number of strokes to the respective index
+    // in either the rows vector or the cols vector.
     for (int i=0; i<k; i++) {
         char rc;
         ll n;
@@ -29,6 +33,8 @@ int32_t main()
             cols[n]++;
         }
     }
+    // go through grid and check if number of strokes is even. if yes, that
+    // cell must be a black square. else, it must be a gold square.
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
             if ((rows[i]+cols[j])%2!=0) {
@@ -36,5 +42,6 @@ int32_t main()
             }
         }
     }
+    // output the gold squares count.
     cout << gold_squares;
 }
