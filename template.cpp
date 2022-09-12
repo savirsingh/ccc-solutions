3052f1b04282e20fcc2f132ca57d5cf2bafd8454
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-12 17:26:54 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,19 @@
+// code by savir singh
+// my cpp template for ccc
+
+#include <bits/stdc++.h>
+#define int long long
+#define del erase
+#define pb push_back
+const auto INF = std::numeric_limits<int>::infinity();
+const int mod = 1000000000000;
+const int max_ = 1000000;
+using namespace std;
+
+// driver code
+int32_t main()
+{
+    // fast i/o
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
