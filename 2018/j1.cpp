650858e9c1467976fd70e727c11b97919d00c434
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-16 19:39:20 -0400

    Create j1.cpp

---
+++ 2018/j1.cpp
@@ -0,0 +1,16 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if ((a==8 || a==9) && (d==8 || d==9) && b==c) {
+        cout << "ignore";
+    }
+    else {
+        cout << "answer";
+    }
+}
