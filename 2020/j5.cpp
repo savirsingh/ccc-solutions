d0a2714ca49d146f9585638404e1dc7144b46773
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-25 19:03:18 -0400

    Create j5.cpp

---
+++ 2020/j5.cpp
@@ -0,0 +1,63 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+// macros
+#define int long long
+#define double long double
+#define bit32 int32_t
+#define del erase
+#define pb push_back
+#define str string
+#define scani(x) scanf("%lld", &x)
+#define scan(x) scanf("%s", &x)
+#define scanc(x) scanf("%c", &x)
+#define printi(x) printf("%lld", x);
+#define print(x) printf("%s", x);
+#define printc(x) printf("%c", x);
+
+class cell {
+public:
+    int r, c;
+};
+
+// all vars
+const int MAX=1000008;
+vector<cell> graph[MAX];
+bool visited[1008][1008];
+
+// driver code
+bit32 main()
+{
+    int m, n;
+    scani(m); scani(n);
+    cell fin;
+    for (int i=1; i<=m; i++) {
+        for (int j=1; j<=n; j++) {
+            int a; cin >> a;
+            cell b;
+            b.r=i; b.c=j;
+            graph[a].pb(b);
+            if (i==m && j==n) {
+                fin=b;
+            }
+        }
+    }
+    deque<cell> deq;
+    deq.pb(fin);
+    while (!deq.empty()) {
+        cell z=deq.front(); deq.pop_front();
+        vector<cell> p = graph[z.r*z.c];
+        for (cell adj : p) {
+            if (adj.r==1 && adj.c==1) {
+                cout << "yes";
+                return false;
+            }
+            else if (!visited[adj.r][adj.c]) {
+                visited[adj.r][adj.c]=true;
+                deq.pb(adj);
+            }
+        }
+    }
+    cout << "no";
+}
