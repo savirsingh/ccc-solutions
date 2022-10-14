99fef9fb91032af09fdbc0b038fd6f6aa40a457c
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-10-14 16:26:52 -0400

    Create mci3.cpp

---
+++ Other Preparation/mci3.cpp
@@ -0,0 +1,37 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+// macros
+#define int long long
+#define double long double
+#define bit32 int32_t
+#define del erase
+#define pb push_back
+#define str string
+#define scani(x) scanf("%lld", &x)
+#define scan(x) scanf("%s", &x)
+#define scanc(x) scanf("%c", &x)
+#define printi(x) printf("%lld", x);
+#define print(x) printf("%s", x);
+#define printc(x) printf("%c", x);
+
+// driver code
+bit32 main()
+{
+    int n; scani(n);
+    vector<char> original;
+    char fin[n];
+    for (int i=0; i<n; i++) {
+        char letter; cin >> letter;
+        original.pb(letter);
+    }
+    for (int i=0; i<n; i++) {
+        int index; cin >> index;
+        index--;
+        fin[index]=original[i];
+    }
+    for (char c : fin) {
+        printc(c);
+    }
+}
