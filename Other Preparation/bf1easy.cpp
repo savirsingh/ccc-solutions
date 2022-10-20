da73b7239a18edbfdd4d0d45420a25a7bf8c4a6a
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-10-20 13:19:13 -0400

    Create bf1easy.cpp

---
+++ Other Preparation/bf1easy.cpp
@@ -0,0 +1,13 @@
+// code by savir singh
+// https://dmoj.ca/problem/bf1easy
+// 100/100
+
+#include <stdio.h>
+
+int main() {
+    int n; scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        printf("%d", i);
+        printf("%c", ' ');
+    }
+}
