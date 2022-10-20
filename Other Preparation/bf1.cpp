ade7d6caa140d221743230f905241557af98b6bf
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-10-20 13:06:38 -0400

    Create bf1.cpp

---
+++ Other Preparation/bf1.cpp
@@ -0,0 +1,11 @@
+// code by savir singh
+
+#include <stdio.h>
+
+int main() {
+    int n; scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        printf("%d", i);
+        printf("%c", ' ');
+    }
+}
