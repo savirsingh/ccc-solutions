ac71e50f9fd116b920e8ba311542fff75110cee0
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-10-22 08:39:06 -0400

    Create sac22cc5jp3.cpp

---
+++ Other Preparation/sac22cc5jp3.cpp
@@ -0,0 +1,28 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+
+using namespace std;
+map<int, set<string>> m;
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    int p, q;
+    cin >> p >> q;
+    while (q--) {
+        int a, b;
+        string c;
+        cin >> a >> b >> c;
+        if (a==2) {
+            m[b].insert(c);
+        }
+        else {
+            if (m[b].find(c)!=m[b].end()) {
+                cout << 1 << "\n";
+            }
+            else {
+                cout << 0 << "\n";
+            }
+        }
+    }
+}
