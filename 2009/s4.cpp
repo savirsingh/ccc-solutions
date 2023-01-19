83a7f74fcd2459553a8783cb51dc3e3fda792b91
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-01-19 18:16:05 -0500

    Update s4.cpp

--- 2009/s4.cpp
+++ 2009/s4.cpp
@@ -1,4 +1,5 @@
 // code by savir singh
+// reversing was a big brain move
 
 #include <bits/stdc++.h>
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
