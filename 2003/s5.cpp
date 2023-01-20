1ab5e73b9b91383cdcffa4685d9662fbc73ff4c8
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-01-19 20:58:02 -0500

    Update s5.cpp

--- 2003/s5.cpp
+++ 2003/s5.cpp
@@ -1,4 +1,6 @@
 // code by savir singh
+// and ladies and gentlemen,
+// this is maximum spanning tree!
 
 #include <bits/stdc++.h>
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
