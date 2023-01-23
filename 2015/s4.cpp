3e2e100a3b62f72f07c278366a3083130efe5d20
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-01-23 18:31:01 -0500

    Update s4.cpp

--- 2015/s4.cpp
+++ 2015/s4.cpp
@@ -1,4 +1,6 @@
 // code by savir singh
+// And this is when I learned that Dijkstra's doesn't require
+// a visited array. Thank you brain.
 
 #include <bits/stdc++.h>
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
