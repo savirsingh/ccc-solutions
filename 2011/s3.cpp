b4481b6b43f77a66b29de4333a3af4bc5909fcf2
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-01-22 20:13:11 -0500

    Update s3.cpp

--- 2011/s3.cpp
+++ 2011/s3.cpp
@@ -1,4 +1,6 @@
 // code by savir singh
+// very simple logic
+// good recursion practice
 
 #include <bits/stdc++.h>
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
