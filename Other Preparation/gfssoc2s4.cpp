fd2d07efca33ce9d6f8ad113a837664eaff7dbe0
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-01-23 07:27:18 -0500

    Update gfssoc2s4.cpp

--- Other Preparation/gfssoc2s4.cpp
+++ Other Preparation/gfssoc2s4.cpp
@@ -1,4 +1,6 @@
 // code by savir singh
+// https://dmoj.ca/problem/gfssoc2s4
+// 100/100
 
 #include <bits/stdc++.h>
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
