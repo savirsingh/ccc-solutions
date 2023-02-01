67861d15804252aac6c80226d2a9ff194f82ce98
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-02-01 17:07:33 -0500

    Update s4.cpp

--- 2017/s4.cpp
+++ 2017/s4.cpp
@@ -1,4 +1,6 @@
 // code by savir singh
+// 15/15 on CCC Grader, 17/17 on DMOJ
+// MST
 
 #include <bits/stdc++.h>
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
