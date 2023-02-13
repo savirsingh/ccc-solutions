4024994ef70a25709d7a53be2b7a9d7d757daa37
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-02-12 21:20:47 -0500

    Create dpc.cpp

---
+++ Other Preparation/dpc.cpp
@@ -0,0 +1,27 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef pair<int, int> pii;
+
+const int MAX = 1e5+2;
+int n, activity[MAX][4], dp[MAX][4];
+
+// dp state:
+// dp[i][j] = total points of happiness by doing first i problems ending on activity j
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin >> n;
+    for (int i=1; i<=n; i++) {cin >> activity[i][1] >> activity[i][2] >> activity[i][3];}
+    for (int i=0; i<=n-1; i++) {
+        for (int j=1; j<=3; j++) {
+            for (int k=1; k<=3; k++) {
+                if (j == k) continue;
+                dp[i+1][k] = max(dp[i+1][k], dp[i][j] + activity[i+1][k]);
+            }
+        }
+    }
+    cout << max(dp[n][1], max(dp[n][2], dp[n][3])) << endl;
+}
