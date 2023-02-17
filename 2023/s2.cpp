5f1a89d43c56329ab2541da9211ddf9fb689ee7c
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-02-17 18:20:06 -0500

    Update s2.cpp

--- 2023/s2.cpp
+++ 2023/s2.cpp
@@ -5,14 +5,13 @@
 using namespace std;
 
 const int MAX = 5008;
-int n, h[MAX], ans[MAX], pre[MAX][MAX];
+int n, h[MAX], pre[MAX][MAX];
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> n;
     for (int i=1; i<=n; i++) cin >> h[i];
-    memset(ans, 0x3f, sizeof ans);
     // calc for all of them
     for (int i=1; i<=n; i++) {
         // window size i
