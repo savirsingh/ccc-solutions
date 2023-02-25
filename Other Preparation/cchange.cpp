03328f7261cee3f8f745e6814114ca627d513a60
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-02-24 19:40:21 -0500

    Create cchange.cpp

---
+++ Other Preparation/cchange.cpp
@@ -0,0 +1,23 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+using namespace std;
+
+const int MAX = 1e4 + 2;
+int x, n, dp[MAX];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    memset(dp, 0x3f, sizeof dp);
+    cin >> x >> n;
+    dp[0] = 0;
+    for (int i = 0; i < n; i++) {
+        int d; cin >> d;
+        for (int j = d; j <= x; j++) {
+            dp[j] = min(dp[j], dp[j - d] + 1);
+        }
+    }
+    cout << dp[x] << endl;
+}
