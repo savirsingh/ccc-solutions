86d59bea24f1ddb5a0d3ea88e4dc6a9dae42cc31
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-01 10:13:42 -0500

    Update dmopc13c3p5.cpp

--- Other Preparation/dmopc13c3p5.cpp
+++ Other Preparation/dmopc13c3p5.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MAX = 152;
-int r, u, m, v[MAX], t[MAX], f[MAX], dp[MAX][302][102];
+int r, u, m, v[MAX], t[MAX], f[MAX], dp[302][102];
 
 int main() {
     iostream::sync_with_stdio(false);
@@ -15,14 +15,11 @@ int main() {
         cin >> v[i] >> t[i] >> f[i];
     }
     for (int i = 1; i <= r; i++) {
-        for (int j = 1; j <= m; j++) {
-            for (int k = 1; k <= u; k++) {
-                dp[i][j][k] = dp[i-1][j][k];
-                if (t[i] <= j && f[i] <= k) {
-                    dp[i][j][k] = max(dp[i][j][k], dp[i-1][j-t[i]][k-f[i]] + v[i]);
-                }
+        for (int j = m; j >= t[i]; j--) {
+            for (int k = u; k >= f[i]; k--) {
+                dp[j][k] = max(dp[j][k], dp[j-t[i]][k-f[i]] + v[i]);
             }
         }
     }
-    cout << dp[r][m][u] << endl;
+    cout << dp[m][u] << endl;
 }
