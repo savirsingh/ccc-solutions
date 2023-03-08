2f1ce5ff8e9cdd42653276c504b4f81e9b50e9bd
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-07 20:49:45 -0500

    Create dmopc18c4p2.cpp

---
+++ Other Preparation/dmopc18c4p2.cpp
@@ -0,0 +1,33 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MAX = 800;
+
+int n, a[MAX], sum = 0;
+bool dp[500000];
+
+// dp[i] = whether we can form the sum i using task times
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    dp[0] = true; // base case
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = sum; j >= a[i]; j--) {
+            if (dp[j-a[i]]) dp[j] = true;
+        }
+    }
+    int best = INT_MAX;
+    for (int i = 0; i <= sum; i++) {
+        if (dp[i]) best = min(best, abs(i - (sum - i)));
+    }
+    cout << best << "\n";
+}
