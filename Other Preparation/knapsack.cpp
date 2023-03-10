cada36dffd03f366297ecd22b6fe8b1fd323eefe
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-10 17:41:51 -0500

    Create

---
+++ Other Preparation/knapsack.cpp
@@ -0,0 +1,29 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MAX = 5005;
+
+int n, m, dp[MAX];
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        int n, v, p; cin >> n >> v >> p;
+        for (int j = 5002; j >= 0; j--) {
+            for (int k = 1; k * v <= j && k <= n; k++) {
+                int cost = k * v, val = k * p;
+                dp[j] = max(dp[j], dp[j-cost] + val);
+            }
+        }
+    }
+    int best = -1e9;
+    for (int i = 1; i <= m; i++) {
+        int c, f; cin >> c >> f;
+        best = max(best, dp[c] - f);
+    }
+    cout << best << endl;
+}
