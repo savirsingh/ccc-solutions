b3d745152e6fd97112b50e73b4e99acf51438aee
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-19 19:03:08 -0400

    Update lis.cpp

--- Other Preparation/lis.cpp
+++ Other Preparation/lis.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 // macros
-#define int long long
+//#define int long long
 #define double long double
 #define bit32 int32_t
 #define del erase
@@ -42,20 +42,40 @@ void read(int &number)
 // all vars
 
 // driver code
-bit32 main() {
-    int N; read(N);
-    int arr[N];
-    vector<int> len;
-    for (int i=0; i<N; i++) {
-        read(arr[i]);
-    }
-    len.pb(arr[0]);
-    for (int i=1; i<N; i++) {
-        if (arr[i]>len[len.size()-1]) len.pb(arr[i]);
+//bit32 main() {
+//    int N; read(N);
+//    int arr[N];
+//    vector<int> len;
+//    for (int i=0; i<N; i++) {
+//        read(arr[i]);
+//    }
+//    len.pb(arr[0]);
+//    for (int i=1; i<N; i++) {
+//        if (arr[i]>len[len.size()-1]) len.pb(arr[i]);
+//        else {
+//           int left=lower_bound(len.begin(), len.end(), arr[i])-len.begin();
+//            len[left]=arr[i];
+//        }
+//    }
+//    cout << len.size() << "\n";
+//}
+
+const int MM = 1e6 + 1;
+int n, a[MM];
+vector<int> lis;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) {
+        if (lis.empty()) lis.push_back(a[i]);
+        else if (a[i] > lis[lis.size()-1]) lis.push_back(a[i]);
         else {
-            int left=lower_bound(len.begin(), len.end(), arr[i])-len.begin();
-            len[left]=arr[i];
+            int index = lower_bound(lis.begin(), lis.end(), a[i]) - lis.begin();
+            lis[index] = a[i];
         }
     }
-    cout << len.size() << "\n";
+    cout << lis.size() << endl;
 }
