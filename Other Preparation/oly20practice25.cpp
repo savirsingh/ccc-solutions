1bdc14c523d013de621303a8b766604229717cb6
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-19 18:58:01 -0400

    Create oly20practice25.cpp

---
+++ Other Preparation/oly20practice25.cpp
@@ -0,0 +1,26 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MM = 1001;
+int n, k;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> k;
+    for (int i = 1, s, t, r; i <= k; i++) {
+        cin >> s >> t >> r;
+        int q = 0, time = 0, ans = 0;
+        while (true) {
+            if (time == t) {
+                time = 0;
+                ans += r;
+            }
+            ans++; time++; q += s;
+            if (q >= n) break;
+        }
+        cout << ans << endl;
+    }
+}
