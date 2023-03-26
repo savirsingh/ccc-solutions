147d88e4e2f456c936597f65ece51c3212fcc509
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-26 16:58:20 -0400

    Create pacnw16a.cpp

---
+++ Other Preparation/pacnw16a.cpp
@@ -0,0 +1,45 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+string s;
+
+int32_t main() {
+    __; cin >> s;
+    vector<int> lis;
+    for (int i = 0; i < sz(s); i++) {
+        if (lis.empty() || s[i] > lis.back()) lis.pb(s[i]);
+        else *lower_bound(all(lis), s[i]) = s[i];
+    }
+    cout << 26 - sz(lis) << endl;
+}
