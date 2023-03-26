23fd57be49a915c210e66695e195b42fa2050886
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-26 17:27:02 -0400

    Create waterloo2017wb_cheese.cpp

---
+++ Other Preparation/waterloo2017wb_cheese.cpp
@@ -0,0 +1,52 @@
+// code by savir singh
+// https://dmoj.ca/problem/waterloo2017wb
+// 100/100
+
+// Approach in this cheese sol is to select last k
+// letters and then either choose a letter that isn't
+// in A or arbitrarily choose a letter (if A contains
+// all letters). Then use DP to check your answer.
+
+// Fails Case:
+// 77 3
+// abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MM = 2001;
+int n, k, dp[MM][MM], last[200];
+string a, b;
+
+bool lcs() {
+    memset(dp, 0, sizeof dp);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[n][n] == k;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> k >> a;
+    if (k > n) {
+        cout << "WRONGANSWER" << endl;
+        return false;
+    }
+    char use = 'l';
+    for (int i = 1; i <= n; i++) last[a[i-1]] = i;
+    for (char j = 'a'; j <= 'z'; j++) {
+        if (last[j] == 0) {
+            use = j;
+            break;
+        }
+    }
+    for (int i = n - k + 1; i <= n; i++) b += a[i-1];
+    for (int i = 1; i <= n - k; i++) b += use;
+    if (!lcs()) cout << "WRONGANSWER" << endl;
+    else cout << b << endl;
+}
