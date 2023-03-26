01ebc025cb173e622af5db2de415abcb1e37da10
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-26 17:25:00 -0400

    Update waterloo2017wb.cpp

--- Other Preparation/waterloo2017wb.cpp
+++ Other Preparation/waterloo2017wb.cpp
@@ -2,42 +2,66 @@
 // https://dmoj.ca/problem/waterloo2017wb
 // 100/100
 
+// I also cheesed this problem (got 100/100 with incorrect sol)
+// check waterloo2017wb_cheese.cpp for the cheese sol
+
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 const int MM = 2001;
-int n, k, dp[MM][MM], last[200];
-string a, b;
+int n, k, freq[30];
+string a;
 
-bool lcs() {
-    memset(dp, 0, sizeof dp);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+int32_t main() {
+    __; cin >> n >> k >> a;
+    f(i, 0, sz(a)) freq[a[i] - 'a']++;
+    char best;
+    int minfreq = INT_MAX;
+    f(i, 0, 26) {
+        if (freq[i] < minfreq) {
+            best = i + 'a';
+            minfreq = freq[i];
         }
     }
-    return dp[n][n] == k;
-}
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cin >> n >> k >> a;
-    if (k > n) {
-        cout << "WRONGANSWER" << endl;
-        return false;
-    }
-    char use = 'l';
-    for (int i = 1; i <= n; i++) last[a[i-1]] = i;
-    for (char j = 'a'; j <= 'z'; j++) {
-        if (last[j] == 0) {
-            use = j;
-            break;
+    if (k < minfreq || k > n) cout << "WRONGANSWER" << endl;
+    else {
+        int so_far = k - minfreq;
+        f(i, 0, n) {
+            if (so_far > 0) {
+                cout << a[i];
+                if (a[i] != best) so_far--;
+            }
+            else cout << best;
         }
+        cout << endl;
     }
-    for (int i = n - k + 1; i <= n; i++) b += a[i-1];
-    for (int i = 1; i <= n - k; i++) b += use;
-    if (!lcs()) cout << "WRONGANSWER" << endl;
-    else cout << b << endl;
 }
