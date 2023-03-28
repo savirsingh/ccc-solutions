7a772ab025bf416e901f0fec2a2a948239e2d229
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-28 19:05:12 -0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,79 +7,39 @@
 #pragma optimize( "", on )
 #include <bits/stdc++.h>
 using namespace std;
-// macros
 #define int long long
-#define double long double
-#define bit32 int32_t
-#define del erase
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
 #define pb push_back
-#define str string
-#define scani(x) scanf("%lld", &x)
-#define scan(x) scanf("%s", &x)
-#define scanc(x) scanf("%c", &x)
-#define printi(x) printf("%lld", x);
-#define print(x) printf("%s", x);
-#define printc(x) printf("%c", x);
 #define pii pair<int, int>
-#define scanner(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-// fast i/o
-void read(int &number)
-{
-    bool negative = false;
-    register int c;
+// declare all your vars here
 
-    number = 0;
-    c = getchar();
-    if (c=='-')
-    {
-        negative = true;
-
-        c = getchar();
-    }
-    for (; (c>47 && c<58); c=getchar())
-        number = number *10 + c - 48;
-    if (negative)
-        number *= -1;
+int32_t main() {
+    __
+        // your code goes here
 }
-
-// all vars
-const int MM = 1e6 + 1;
-
-// driver code
-bit32 main() {
-  
-}
-
-/*
- .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
-| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
-| |    _______   | || |      __      | || | ____   ____  | || |     _____    | || |  _______     | |
-| |   /  ___  |  | || |     /  \     | || ||_  _| |_  _| | || |    |_   _|   | || | |_   __ \    | |
-| |  |  (__ \_|  | || |    / /\ \    | || |  \ \   / /   | || |      | |     | || |   | |__) |   | |
-| |   '.___`-.   | || |   / ____ \   | || |   \ \ / /    | || |      | |     | || |   |  __ /    | |
-| |  |`\____) |  | || | _/ /    \ \_ | || |    \ ' /     | || |     _| |_    | || |  _| |  \ \_  | |
-| |  |_______.'  | || ||____|  |____|| || |     \_/      | || |    |_____|   | || | |____| |___| | |
-| |              | || |              | || |              | || |              | || |              | |
-| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
- '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
-*/
-
-// - savir
-/*
-       /\     /\
-      //\\___//\\
-      \_  o o  _/
-       /   ^   \
-      /  '---'  \
-     |__|_|_|__|
-     |  | | |  |
-     |__|_|_|__|
-    /'-./  | \.-'\
-   /   /   |   \   \
-  (`-/-`)  |  (`-/-`)
-   `"`"`"``|``"`"`"`
-          \|/
-           V
-*/
