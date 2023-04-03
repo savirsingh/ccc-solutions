6e9884032874564198f72ea53c964c5c3de9a011
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-03 16:54:28 -0400

    Create dph.cpp

---
+++ Other Preparation/dph.cpp
@@ -0,0 +1,80 @@
+// code by savir singh
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MM = 1001;
+int h, w, grid[MM][MM], dp[MM][MM];
+
+// fast i/o
+void read(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+    c = getchar();
+    if (c=='-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+    for (; (c>47 && c<58); c=getchar())
+        number = number *10 + c - 48;
+    if (negative)
+        number *= -1;
+}
+
+int32_t main() {
+    __ cin >> h >> w;
+    fe(i, 1, h) {
+        fe(j, 1, w) {
+            char c; cin >> c;
+            if (c == '#') grid[i][j] = 1;
+        }
+    }
+    dp[1][1] = 1;
+    fe(i, 1, h) {
+        fe(j, 1, w) {
+            if (grid[i][j]) continue;
+            // check up
+            dp[i][j] += dp[i-1][j];
+            // check left
+            dp[i][j] += dp[i][j-1];
+            dp[i][j] %= mod;
+        }
+    }
+    cout << dp[h][w] << endl;
+}
