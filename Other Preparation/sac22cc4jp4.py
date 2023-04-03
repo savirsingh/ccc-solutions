ce676a8893eab529e9c684397c2d579e357bbce3
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-03 19:59:09 -0400

    Create sac22cc4jp4.py

---
+++ Other Preparation/sac22cc4jp4.py
@@ -0,0 +1,19 @@
+# code by savir singh
+
+n, k = map(int, input().split())
+p = list(map(int, input().split()))
+
+nxt = [[0] * n for _ in range(62)]
+for i in range(n):
+    nxt[0][p[i]-1] = i
+for i in range(1, 62):
+    for j in range(n):
+        nxt[i][j] = nxt[i-1][nxt[i-1][j]]
+
+a = p
+for i in range(61, -1, -1):
+    if k >= 2**i:
+        a = [nxt[i][x-1]+1 for x in a]
+        k -= 2**i
+
+print(*a)
