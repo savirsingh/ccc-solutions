dd7587cae6e62e36e9c69d626d07e2db64cd55f8
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-03 21:44:54 -0400

    Create ioi00p1.cpp

---
+++ Other Preparation/ioi00p1.cpp
@@ -0,0 +1,79 @@
+// code by savir singh
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MM = 5001;
+int n;
+vector<int> dp_cur, dp_prev;
+char a[MM], b[MM];
+
+// fast i/o
+void read(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+    c = getchar();
+    if (c=='-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+    for (; (c>47 && c<58); c=getchar())
+        number = number *10 + c - 48;
+    if (negative)
+        number *= -1;
+}
+
+int32_t main() {
+    __ cin >> n;
+    dp_cur.resize(n + 1);
+    dp_prev.resize(n + 1);
+    fe(i, 1, n) cin >> a[i];
+    fe(i, 1, n) b[i] = a[n-i+1];
+    fe(i, 1, n) {
+        fe(j, 1, n) {
+            if (a[i] == b[j]) {
+                dp_cur[j] = dp_prev[j-1] + 1;
+            }
+            else dp_cur[j] = max(dp_cur[j-1], dp_prev[j]);
+        }
+        swap(dp_cur, dp_prev);
+        dp_cur.assign(n+1, 0);
+    }
+    cout << n - dp_prev[n] << endl;
+}
