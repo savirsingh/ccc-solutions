79a07ab0d3a658c58267b90b003ed2535c135c96
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-04 18:25:41 -0400

    Create dmopc14c4p3.cpp

---
+++ Other Preparation/dmopc14c4p3.cpp
@@ -0,0 +1,73 @@
+// code by savir singh
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MM = 1001;
+int l, d, grid[MM][MM], dp[MM][MM], x, y;
+
+// fast i/o
+void read(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+    c = getchar();
+    if (c=='-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+    for (; (c>47 && c<58); c=getchar())
+        number = number *10 + c - 48;
+    if (negative)
+        number *= -1;
+}
+
+int32_t main() {
+    __ read(d); read(l);
+    fe(i, 1, l) fe(j, 1, d) read(grid[i][j]);
+    read(x); read(y);
+    fe(i, 0, l) fe(j, 0, d) dp[i][j] = 1e9;
+    dp[1][1] = grid[1][1];
+    fe(i, 1, l) {
+        fe(j, 1, d) dp[i][j] = min(dp[i][j], grid[i][j] + min({dp[i-1][j], dp[i][j-1]}));
+        f(j, 1, d) {
+            dp[i][j] = min(dp[i][j], dp[i][j + 1] + grid[i][j]);
+        }
+    }
+    cout << dp[y+1][x+1] << endl;
+}
