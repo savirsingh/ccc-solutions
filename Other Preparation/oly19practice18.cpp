afa7a57b38b48417e0a56c1d653b2ccb688e7c5a
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-10 16:03:58 -0400

    Update oly19practice18.cpp

--- Other Preparation/oly19practice18.cpp
+++ Other Preparation/oly19practice18.cpp
@@ -1,4 +1,7 @@
 // code by savir singh
+// https://dmoj.ca/problem/oly19practice18
+// 100/100
+// Olympiads Homework
 
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
