adc0940726d27456d0ffdb4359dcab9a6dc7f18c
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-10 15:13:07 -0400

    Create coci06c5p5.cpp

---
+++ Other Preparation/coci06c5p5.cpp
@@ -0,0 +1,76 @@
+// code by savir singh
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MM = 201;
+int n, arr[MM], dp[MM][MM], ans = 0;
+
+// fast i/o
+void read(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+    c = getchar();
+    if (c=='-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+    for (; (c>47 && c<58); c=getchar())
+        number = number *10 + c - 48;
+    if (negative)
+        number *= -1;
+}
+
+int32_t main() {
+    __ read(n);
+    fe(i, 1, n) {
+        read(arr[i]);
+        arr[i] %= 2;
+        dp[i+n][i+n] = dp[i][i] = arr[i];
+    }
+    fe(adder, 1, n) {
+        fe(l, 1, 2 * n) {
+            int r = l + adder;
+            if (r > 2 * n) break;
+            dp[l][r] = max(dp[l][l] - dp[l+1][r], dp[r][r] - dp[l][r-1]);
+        }
+    }
+    fe(i, 1, n) if (dp[i][i] - dp[i+1][i+n-1] > 0) ans++;
+    cout << ans << endl;
+}
