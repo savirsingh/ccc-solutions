87ae32b8ee56c493f0e31551f8de1225d8f8739a
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-16 22:39:15 -0400

    Create ecoo15r1p1.cpp

---
+++ Other Preparation/ecoo15r1p1.cpp
@@ -0,0 +1,84 @@
+// code by savir singh
+// https://dmoj.ca/problem/ecoo15r1p1
+// 100/100
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MM = 201;
+map<string, int> colour;
+set<string> cols;
+
+// fast i/o
+void read(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+    c = getchar();
+    if (c=='-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+    for (; (c>47 && c<58); c=getchar())
+        number = number *10 + c - 48;
+    if (negative)
+        number *= -1;
+}
+
+int32_t main() {
+    __ int t = 10;
+    while(t--) {
+        int ans = 0;
+        colour.clear();
+        cols.clear();
+        while(true) {
+            string s;
+            getline(cin, s);
+            if (s == "end of box") break;
+            cols.insert(s);
+            colour[s]++;
+        }
+        for (auto i : cols) {
+            int hf = colour[i] / 7;
+            if (colour[i] % 7) hf++;
+            if (i == "red") ans += colour[i] * 16;
+            else ans += hf * 13;
+        }
+        cout << ans << endl;
+    }
+}
