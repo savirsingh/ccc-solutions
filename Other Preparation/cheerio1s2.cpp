86810774b7697b0f9646267eebb96ee5ccdb5ba4
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-16 22:41:15 -0400

    Create cheerio1s2.cpp

---
+++ Other Preparation/cheerio1s2.cpp
@@ -0,0 +1,73 @@
+// code by savir singh
+// https://dmoj.ca/problem/cheerio1s2
+// 15/15
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MM = 2e5 + 1;
+int n, a, b, ans = 0;
+
+// fast i/o
+void read(int &number){
+    bool negative = false;
+    register int c;
+
+    number = 0;
+    c = getchar();
+    if (c=='-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+    for (; (c>47 && c<58); c=getchar())
+        number = number *10 + c - 48;
+    if (negative)
+        number *= -1;
+}
+
+int32_t main() {
+    __ read(n); read(a); read(b);
+    priority_queue<vi, vector<vi>, greater<vi>> pq;
+    pq.push({a + b, 1, 1});
+    f(i, 0, n) {
+        auto task = pq.top();
+        pq.pop();
+        ans += task[0];
+        pq.push({a * task[1] + b * (task[2] + 1), task[1], task[2] + 1});
+        if (task[2] == 1) pq.push({a * (task[1] + 1) + b, task[1] + 1, 1});
+    }
+    cout << ans << endl;
+}
