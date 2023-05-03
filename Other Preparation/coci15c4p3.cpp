40d3afc2f81a299a8c66e406731bb27da2211146
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-05-02 21:08:05 -0400

    Create coci15c4p3.cpp

---
+++ Other Preparation/coci15c4p3.cpp
@@ -0,0 +1,58 @@
+// code by savir singh
+// https://dmoj.ca/problem/coci15c4p3
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MM = 1001;
+int n, a[MM], m[MM][MM];
+
+int32_t main() {
+    __ cin >> n;
+    fe(i, 1, n)
+    {
+        fe(j, 1, n) cin >> m[i][j];
+    }
+    fe(i, 1, n)
+    {
+        int op = m[i][1];
+        fe(j, 2, n)
+        {
+            op |= m[i][j];
+        }
+        a[i] = op;
+    }
+    f(i, 1, n) cout << a[i] << " ";
+    cout << a[n] << endl;
+}
