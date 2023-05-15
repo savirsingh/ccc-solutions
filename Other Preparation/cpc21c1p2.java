b03fe6dbe434845ad85f663e8c9eadbb28661e96
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-05-15 18:49:05 -0400

    Create cpc21c1p2.java

---
+++ Other Preparation/cpc21c1p2.java
@@ -0,0 +1,45 @@
+// code by savir singh
+
+import java.util.*;
+
+public class Main {
+    static final long MM = 200001L;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        n *= 2;
+        n += 1;
+        long ret = 0;
+        List<Long> a = new ArrayList<>();
+        List<Long> b = new ArrayList<>();
+
+        for (long i = 0; i < n; i++) {
+            long x = scanner.nextLong();
+            ret ^= x;
+            a.add(x);
+        }
+
+        for (long i = 0; i < n; i++) {
+            long x = scanner.nextLong();
+            ret ^= x;
+            b.add(x);
+        }
+
+        for (long i = 0; i < n; i++) {
+            b.set((int) i, b.get((int) i) ^ ret);
+        }
+
+        Collections.sort(a);
+        Collections.sort(b);
+
+        for (long i = 0; i < n; i++) {
+            if (!a.get((int) i).equals(b.get((int) i))) {
+                ret = -1;
+                break;
+            }
+        }
+
+        System.out.println(ret);
+    }
+}
