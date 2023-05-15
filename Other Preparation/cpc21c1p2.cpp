ec28cf471971a45233ea572912ec2b07928ae7ff
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-05-15 18:45:29 -0400

    Create cpc21c1p2.cpp

---
+++ Other Preparation/cpc21c1p2.cpp
@@ -0,0 +1,27 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+const int MM = 2e5 + 1;
+int n, ret;
+vector<int> a, b;
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;n*=2;n+=1;
+    for (int i = 0, x; i < n; i++) {
+        cin >> x; ret ^= x; a.push_back(x);
+    }
+    for (int i = 0, x; i < n; i++) {
+        cin >> x; ret ^= x; b.push_back(x);
+    }
+    for (int i = 0 ; i < n; i++) b[i] ^= ret;
+    sort(a.begin(), a.end());sort(b.begin(), b.end());
+    for (int i = 0 ; i < n; i++) {
+        if (a[i] != b[i]) {ret = -1;break;}
+    }
+    cout << ret << endl;
+}
