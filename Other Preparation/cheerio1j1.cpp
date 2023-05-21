230d8960472cb6a9f32268b72cf4acb1c821563b
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-05-21 17:14:44 -0400

    Create cheerio1j1.cpp

---
+++ Other Preparation/cheerio1j1.cpp
@@ -0,0 +1,22 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isvowel(char c) {
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+    else return false;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s; cin >> s;
+    set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    for (int i = 1; i < s.size(); i++) {
+        if ((isvowel(s[i]) && isvowel(s[i-1])) || (!isvowel(s[i]) && !isvowel(s[i-1]))) {
+            cout << "NO\n";return false;
+        }
+    }
+    cout << "YES\n";
+}
