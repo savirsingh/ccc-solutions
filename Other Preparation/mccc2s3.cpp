a3458c70552232f0e64d27d70b59ca164d5ea88b
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-05-23 21:51:08 -0400

    Update mccc2s3.cpp

--- Other Preparation/mccc2s3.cpp
+++ Other Preparation/mccc2s3.cpp
@@ -1,4 +1,6 @@
 // code by savir singh
+// https://dmoj.ca/problem/mccc2s3
+// 15/15
 
 #include <bits/stdc++.h>
 #define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
