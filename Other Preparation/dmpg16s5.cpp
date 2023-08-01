d70957fa27e86a9b09d7b28e81a657166650bd90
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-08-01 19:21:53 -0400

    Update dmpg16s5.cpp

--- Other Preparation/dmpg16s5.cpp
+++ Other Preparation/dmpg16s5.cpp
@@ -92,3 +92,95 @@ signed main() {
 
     return 0;
 }
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//// Compiler Optimizations
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//
+//// Shorthand Macros
+//#define bit32 int32_t
+//#define int long long // comment out if unnecessary
+//#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+//#define endl '\n'
+//#define pb push_back
+//#define pii pair<int, int>
+//#define vi vector<int>
+//#define vvi vector<vi>
+//#define vvvi vector<vvi>
+//#define vpii vector<pii>
+//#define vvpii vector<vpii>
+//#define all(v) (v).begin(), (v).end()
+//#define rall(v) (v).rbegin(), (v).rend()
+//#define sz(a) (int) (a).size()
+//#define rsz resize
+//#define eb emplace_back
+//#define f(i,x,n) for(int i=x;i<n;i++)
+//#define fe(i,x,n) for(int i=x;i<=n;i++)
+//#define fr(i,x,n) for(int i=x;i>n;i--)
+//#define fre(i,x,n) for(int i=x;i>=n;i--)
+//#define mod 1000000007
+//#define mod2 998244353
+//#define INF 1e18
+//#define ld long double
+//#define setbits(x) __builtin_popcountll(x)
+//#define zrobits(x) __builtin_ctzll(x)
+//#define ps(x,y) fixed<<setprecision(y)<<x
+//#define w(x) int x; cin>>x; while(x--)
+//mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+//
+//// Fast Input
+//template<typename T, typename... Args>
+//inline void fastscan(T& num, Args&... args) {
+//    int c = getchar();
+//    num = 0;
+//    bool neg = false;
+//
+//    // Handle negative numbers
+//    if (c == '-') {
+//        neg = true;
+//        c = getchar();
+//    }
+//
+//    // Process digits
+//    while (c >= '0' && c <= '9') {
+//        num = num * 10 + (c - '0');
+//        c = getchar();
+//    }
+//
+//    if (neg)
+//        num = -num;
+//
+//    if constexpr (sizeof...(args) > 0)
+//        fastscan(args...); // Recursively read the remaining arguments
+//}
+//
+//// == your template ends here, start coding!!! ==
+//
+//
+////vars
+//const int mm = 1e5 + 1;
+//int n, k, a[mm * 2], psa[mm * 2], ans = 0;//at least one (+)
+//
+//bit32 main() {
+//    __
+//    fastscan(n, k);
+//    fe(i, 1, n) fastscan(a[i]);
+//    //for circle just double the array
+//    fe(i, n + 1, 2 * n) a[i] = a[i - n];
+//    fe(i, 1, 2 * n) psa[i] = psa[i - 1] + a[i];
+//    deque<pii> deq;
+//    fe(i, 1, 2 * n) {
+//        while (i > k && !deq.empty() && deq.front().first < i - k)
+//            deq.pop_front();
+//        while (!deq.empty() && deq.back().second > psa[i])
+//            deq.pop_back();
+//        deq.pb({i, psa[i]});
+//        ans = max(ans, psa[i] - deq.front().second);
+//    }
+//    cout << ans << endl;
+//
+//    return 0;
+//}
