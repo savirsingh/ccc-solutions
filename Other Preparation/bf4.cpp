61609ea305fac2c4e178adf6f142c6eeca324642
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-08-14 00:57:59 -0400

    Create bf4.cpp

---
+++ Other Preparation/bf4.cpp
@@ -0,0 +1,127 @@
+// code by savir singh
+
+// Include everything needed for CP
+#include <bits/stdc++.h>
+using namespace std;
+
+// Compiler Optimizations
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+// Shorthand Macros
+#define bit32 int32_t
+#define int long long // comment out if unnecessary
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; fastscan(x); while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// Fast Input
+template<typename T, typename... Args>
+inline void fastscan(T& num, Args&... args) {
+    int c = getchar();
+    num = 0;
+    bool neg = false;
+
+    // Handle negative numbers
+    if (c == '-') {
+        neg = true;
+        c = getchar();
+    }
+
+    // Process digits
+    while (c >= '0' && c <= '9') {
+        num = num * 10 + (c - '0');
+        c = getchar();
+    }
+
+    if (neg)
+        num = -num;
+
+    if constexpr (sizeof...(args) > 0)
+        fastscan(args...); // Recursively read the remaining arguments
+}
+
+// Fast Output For Integers
+inline void fastprint(int num, const string& endline = "\n") {
+    if (num < 0) {
+        putchar('-');
+        num = -num;
+    }
+    char buffer[20];
+    int idx = 19;
+    buffer[idx--] = '\0';
+
+    do {
+        buffer[idx--] = static_cast<char>('0' + num % 10);
+        num /= 10;
+    } while (num > 0);
+
+    fputs(&buffer[idx + 1], stdout);
+    fputs(endline.c_str(), stdout);
+}
+
+// Fast Output for Strings
+inline void fastprint(const string& s, const string& endline = "\n") {
+    fputs(s.c_str(), stdout);
+    fputs(endline.c_str(), stdout);
+}
+
+// == your template ends here, start coding!!! ==
+
+const int mm = 1e6 + 1;
+string s, t;
+int n, m, h[mm], p[mm], pt = 0, thash, A = 10007;
+
+inline int getHash(int a, int b) {
+    return (h[b] - h[a - 1] * p[b - a + 1] % mod + mod) % mod;
+}
+
+bit32 main() {
+    __
+    p[0] = 1;
+    cin >> s >> t;
+    n = sz(s), m = sz(t);
+    f(i, 0, m) thash = (thash * A + t[i]) % mod;
+    h[0] = s[0];
+    f(i, 1, n) {
+        p[i] = (p[i - 1] * A) % mod;
+        h[i] = (h[i - 1] * A + s[i]) % mod;
+    }
+
+    while (pt + m <= n) {
+        int substringHash = getHash(pt, pt + m - 1);
+        if (substringHash == thash) {
+            fastprint(pt);
+            return 0;
+        }
+        ++pt;
+    }
+
+    fastprint(-1);
+
+    return 0;
+}
