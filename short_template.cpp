3f4e0763872e425e25cb49a1e224b4599c4b0a15
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-09-11 20:55:52 -0400

    Create short_template.cpp

---
+++ short_template.cpp
@@ -0,0 +1,36 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Define commonly used macros
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define boost() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define debug(x) cerr << #x << " = " << x << endl
+
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+int power(int x, int y) {
+        int res = 1;x %= MOD;if (x == 0) return 0;while (y > 0) {if (y & 1) res = (res * x) % MOD;y >>= 1;x = (x * x) % MOD;}
+        return res;
+}
+
+int gcd(int a, int b) {
+        if (b == 0) return a;return gcd(b, a % b);
+}
+
+int32_t main() {
+        boost();
+        // Your code here
+
+        return 0;
+}
