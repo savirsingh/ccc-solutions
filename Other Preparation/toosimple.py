9c7a9f7f5f7b85bb16022ab7c52418ec3b7582dd
Savir Singh <savirsaroha@gmail.com>
2023-10-30 21:11:06 -0400

    Create toosimple.py

---
+++ Other Preparation/toosimple.py
@@ -0,0 +1,44 @@
+# code by savir singh
+
+val=True
+forh = val + val
+forh *= (val+val+val)
+forh *= (val+val) * (val+val+val)
+forh *= (val+val)
+first=forh
+forh = val+val
+forh *= forh
+forh *= forh
+forh *= (val+val+val+val+val+val)
+forh += (val+val)*(val+val+val)
+forh -= val
+second=forh
+forh = (val+val+val)**(val+val)
+forh *= (val+val+val)*(val+val+val+val)
+third=forh
+fourth=forh
+forh += val+val+val
+valll=forh
+fifth=forh
+forh = val+val+val+val
+forh *= val+val+val+val+val+val+val+val+val+val+val
+sixth=forh
+vall = forh
+forh = val+val
+forh **= (val+val+val+val+val)
+seventh=forh
+forh = vall * (val+val)
+forh -= val
+eighth=forh
+eleventh=valll
+twelfth=valll+val+val+val
+thirteenth=valll-val-val-val
+forh = val+val+val+val+val
+forh *= (val+val)
+forh *= forh
+fourteenth=forh
+forh = val+val
+forh **= (val+val+val+val+val)
+forh += val
+fifteenth=forh
+print(chr(first)+chr(second)+chr(third)+chr(fourth)+chr(fifth)+chr(sixth)+chr(seventh)+chr(eighth)+chr(eleventh)+chr(twelfth)+chr(thirteenth)+chr(fourteenth)+chr(fifteenth))
