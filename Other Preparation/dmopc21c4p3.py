7ed0b978720e67b545c233c817636813d740ef0c
Savir Singh (Saroha) <savirsaroha@gmail.com>
2024-11-26 23:26:32 -0500

    Create dmopc21c4p3.py

---
+++ Other Preparation/dmopc21c4p3.py
@@ -0,0 +1,8 @@
+# code by savir singh
+# https://dmoj.ca/problem/dmopc21c4p3
+# 100/100
+
+numbers = [10001, 10101, 10201, 10301, 11011, 12021, 13031, 11111, 11211, 0]
+
+for number in numbers:
+    print(number)
